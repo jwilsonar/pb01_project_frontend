a53fd146575a9e88baa21070192eab99d1208fc1
jwilsonar <wilsonalbornozdev@gmail.com>
2025-03-09 23:56:03 -0500

    Corrección de middleware para rutas protegidas

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,47 +3,39 @@ import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 
-export default async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
-  
-  // Si el usuario está autenticado y trata de acceder a la página principal
-  if (token && req.nextUrl.pathname === '/') {
-    const isHR = token.is_hr as boolean;
-    return NextResponse.redirect(new URL(isHR ? '/empleados' : '/mi-perfil', req.url));
-  }
-
-  // Para las rutas protegidas
-  const authMiddleware = await withAuth(
-    async function middleware(req) {
-      const token = await getToken({ req });
-      if (!token) return NextResponse.redirect(new URL('/', req.url));
-
+export default withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req });
+    
+    // Si el usuario está autenticado y trata de acceder a la página principal
+    if (token && req.nextUrl.pathname === '/') {
       const isHR = token.is_hr as boolean;
-      const path = req.nextUrl.pathname;
-
-      // Restricción de acceso basada en el rol
-      if (isHR && path.startsWith('/mi-perfil')) {
-        return NextResponse.redirect(new URL('/empleados', req.url));
-      }
-
-      if (!isHR && path.startsWith('/empleados')) {
-        return NextResponse.redirect(new URL('/mi-perfil', req.url));
-      }
-
-      return NextResponse.next();
-    },
-    {
-      callbacks: {
-        authorized: ({ token }) => !!token,
-      },
-      pages: {
-        signIn: '/',
-      },
+      return NextResponse.redirect(new URL(isHR ? '/empleados' : '/mi-perfil', req.url));
     }
-  );
 
-  return authMiddleware(req, NextResponse.next());
-}
+    const isHR = token?.is_hr as boolean;
+    const path = req.nextUrl.pathname;
+
+    // Restricción de acceso basada en el rol
+    if (isHR && path.startsWith('/mi-perfil')) {
+      return NextResponse.redirect(new URL('/empleados', req.url));
+    }
+
+    if (!isHR && path.startsWith('/empleados')) {
+      return NextResponse.redirect(new URL('/mi-perfil', req.url));
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: '/',
+    },
+  }
+);
 
 export const config = {
   matcher: [
